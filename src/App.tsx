ebd5879db228c8c7bdd333fca035ad539baf0c2b
RajeshGajengi <gajengi95@gmail.com>
2026-03-02 11:22:58 +0530

    browserrouter update

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/Hayat-Gym/>
+      <BrowserRouter basename="/Hayat-Gym/">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
